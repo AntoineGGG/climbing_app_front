d7c544ec98d3d3fa4ce542479f00480404b4ece1
AntoineGGG <a.gorenflot@gmx.fr>
2021-02-04 00:01:18 +0100

    start to solve fusion pb

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
+import { Switch, Route } from 'react-router-dom';
+import { Router } from 'react-router';
+
 import './App.css';
+import Login from './Login';
+import Navbar from './Navbar';
+import CragRoutes from './CragRoutes';
+import Crags from './Crags';
+import history from '../history';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Router history={history}>
+        <Navbar />
+        <Switch>
+          <Route exact path='/' component={Login} />
+          <Route exact path='/routes' component={CragRoutes} />
+
+          <Route exact path='/crags' component={Crags} />
+        </Switch>
+      </Router>
     </div>
   );
 }
